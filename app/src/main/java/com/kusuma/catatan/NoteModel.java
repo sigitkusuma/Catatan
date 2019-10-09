eabc7bc02184f0000d418e5a77ed06bce5a2f770
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-09 16:03:23 +0700

    Initial commit

---
+++ app/src/main/java/com/kusuma/catatan/NoteModel.java
@@ -0,0 +1,27 @@
+package com.kusuma.catatan;
+/**
+ * Created by Sigit Kusuma on 11/26/2017.
+ */
+public class NoteModel {
+    public String noteTitle;
+    public String noteTime;
+
+    public NoteModel(){
+    }
+    public NoteModel(String noteTitle, String noteTime) {
+        this.noteTitle = noteTitle;
+        this.noteTime = noteTime;
+    }
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+    public String getNoteTime() {
+        return noteTime;
+    }
+    public void setNoteTime(String noteTime) {
+        this.noteTime = noteTime;
+    }
+}
